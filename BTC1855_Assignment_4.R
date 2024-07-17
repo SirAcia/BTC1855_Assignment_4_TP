29aa1d318957fdc95aa6d9463633e92c274cd5b9
Trinley Palmo <t.palmo@hotmail.com>
2024-07-17 18:49:07 -0400

    Changed class type of sighting datetime column

--- BTC1855_Assignment_4.R
+++ BTC1855_Assignment_4.R
@@ -56,4 +56,6 @@ summary(ufo)
 #' which is the date and time of sighting.
 names(ufo)[names(ufo) == 'datetime'] <- 'sighting_datetime'
 
-
+#' Convert values in sighting_datetime to datetime objects for easy manipulation
+#' later on
+ufo$sighting_datetime <- strptime(ufo[["sighting_datetime"]],"%Y-%m-%d %H:%M")
