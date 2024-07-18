b0dfc1826f3e857e9e0ee9871238f97d84acaa76
Trinley Palmo <t.palmo@hotmail.com>
2024-07-17 22:55:52 -0400

    Fixed character to datetime object function bug

--- BTC1855_Assignment_4.R
+++ BTC1855_Assignment_4.R
@@ -20,14 +20,17 @@
 #' Step 9: Create a new column called report_delay. It should contain the time
 #' differences in days between date of sighting and date reported.
 #' Step 10: Remove the rows where the date of sighting > date reported.
-#' Step 11: Extract all rows where there is no missing data in country, duration
-#' seconds, and report_delay.
-#' Step 12: Create a table with the average report_delay per country (Group by
+#' Step 11: Create a table with the average report_delay per country (Group by
 #' country).
-#' Step 13: Create a histogram using the 'duration seconds' column.
+#' Step 12: Create a histogram using the 'duration seconds' column.
+
+# Install packages if you don't already have them installed
+# install.packages("dplyr")
+# install.packages("lubridate")
 
 # Load packages needed to clean and analyze data
 library(dplyr)
+library(lubridate)
 
 # Set the working directory for where you can find the ufo dataset
 setwd("C:/Users/tpalm/Downloads/")
@@ -53,15 +56,17 @@ summary(ufo)
 #' Change the datetime column name to `sighting_datetime`. Currently it is called
 #' `datetime`, which is an ambiguous name. It should reflect what it contains,
 #' which is the date and time of sighting.
-names(ufo)[names(ufo) == 'datetime'] <- 'sighting_datetime'
+names(ufo)[names(ufo) == "datetime"] <- "sighting_datetime"
 
 # Check if the name has been updated
 colnames(ufo)
 
 #' Convert values in sighting_datetime and date_posted to POSIXlt objects for 
 #' easy manipulation later on
-ufo$sighting_datetime <- strptime(ufo[["sighting_datetime"]],"%Y-%m-%d %H:%M")
-ufo$date_posted <- strptime(ufo[["date_posted"]], "%d-%m-%Y")
+ufo$sighting_datetime <- parse_date_time(ufo[["sighting_datetime"]],
+                                         "%Y-%m-%d %H:%M", exact = TRUE)
+ufo$date_posted <- parse_date_time(ufo[["date_posted"]], "%d-%m-%Y",
+                                   exact = TRUE)
 
 #' Although some of the missing values in the `country` column can be filled in
 #' with the correct country based on the `city` column values, there are too many
